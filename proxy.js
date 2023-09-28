acf4e9e765204639e284572de174e72723acac56
Arpitha Murthy <arpithad@microsoft.com>
2023-09-28 13:49:54 +0530

    Initial commit

---
+++ proxy.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const axios = require("axios");
+const cors = require("cors");
+
+const app = express();
+const port = 3000;
+
+app.use(express.json());
+app.use(cors())
+
+// Define a route that proxies the request to the target API
+app.post("/proxy", async (req, res) => {
+  try {
+    const { url, method, headers, data } = req.body;
+    const response = await axios({
+      url,
+      method,
+      headers,
+      data,
+    });
+    res.json(response.data);
+  } catch (error) {
+    console.error("Proxy Error:", error);
+    res.status(500).json({ error: "Proxy Error" });
+  }
+});
+
+app.listen(port, () => {
+  console.log(`Proxy server is running on port ${port}`);
+});
